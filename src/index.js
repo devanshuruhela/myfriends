08a19d5f317acbf66b77e422836784a594078b05
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-07-07 13:03:57 +0530

    updating with tachyons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './Hello';
+import 'tachyons'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello/>
   </React.StrictMode>,
   document.getElementById('root')
 );
