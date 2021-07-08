82fbf7c43b23935e19d3ef0537761a90ef9cf607
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2021-07-09 01:01:31 +0530

    Added fetch friends from website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React , {Component} from 'react';
 import CardList from './CardList'
-import { friend } from './friend';
 import SearchBox from './SearchBox';
 
 
@@ -11,7 +10,7 @@ class App extends Component
     super()
     this.state=
     {
-      friend : friend,
+      friend : [],
       searchfield:''
     }
   }
@@ -19,10 +18,25 @@ class App extends Component
     this.setState({searchfield : event.target.value})
     //console.log(filternames);
   }
+
+  componentDidMount()
+  {
+    fetch('https://jsonplaceholder.typicode.com/users').then(Response=>{
+      return Response.json();
+    }).then(users=>
+      {
+        this.setState({friend:users})
+      })
+  }
+
 render(){
     const filternames = this.state.friend.filter(frien=>{
       return frien.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
+    if(this.state.friend.lenght===0)
+    {
+      return <h1>Loading...</h1>
+    }
   return(
     <div className='tc'>
       <h1>My Friends</h1>
